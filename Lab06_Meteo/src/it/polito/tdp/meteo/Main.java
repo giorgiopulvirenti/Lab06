1f066e257463eaa0158e9fcdb86f5d3f4b24764c
giorgio <giorgio@pc-Giorgio>
2017-04-28 19:20:51 +0200

    www

--- Lab06_Meteo/src/it/polito/tdp/meteo/Main.java
+++ Lab06_Meteo/src/it/polito/tdp/meteo/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
 			MeteoController controller = loader.getController();
-			// TODO impostare il model nel controller
+			// TODO impostare il model nel controlle
 
 			primaryStage.setScene(scene);
 			primaryStage.show();
